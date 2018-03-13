45621a9cdce568f1d5a54e6728cd4e99f59d5340
Travis Martin <travislovespie@yahoo.com>
2018-03-13 12:24:52 -0400

    updated normal vs. on drugs simulation

--- WPFApp2/normalVsOnDrugsControl.xaml.cs
+++ WPFApp2/normalVsOnDrugsControl.xaml.cs
@@ -25,6 +25,16 @@ namespace WpfApp2
         public normalVsOnDrugsControl()
         {
             InitializeComponent();
+
+            leftImage.Visibility = Visibility.Visible;
+            rightImage.Visibility = Visibility.Visible;
+            drugLabel.Visibility = Visibility.Visible;
+            rehabLabel.Visibility = Visibility.Hidden;
+            drugButton.Visibility = Visibility.Hidden;
+            rehabButton.Visibility = Visibility.Visible;
+            rehabImageRight.Visibility = Visibility.Hidden;
+            rehabImageLeft.Visibility = Visibility.Hidden;
+            rehabSlider.Visibility = Visibility.Hidden;
         }
 
         public static normalVsOnDrugsControl Instance
@@ -44,6 +54,88 @@ namespace WpfApp2
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            String drugListValue = (drugList.SelectedItem as ComboBoxItem).Content.ToString();
+            if (drugListValue == "Alcohol")
+            {
+                leftImage.Visibility = Visibility.Hidden;
+                leftImage.Source = new BitmapImage(new Uri("Resources/non_alcoholic_brain.png", UriKind.Relative));
+                leftImage.Visibility = Visibility.Visible;
+
+                rightImage.Visibility = Visibility.Hidden;
+                rightImage.Source = new BitmapImage(new Uri("Resources/alcoholic_brain.png", UriKind.Relative));
+                rightImage.Visibility = Visibility.Visible;
+            } else if (drugListValue == "Smoker")
+            {
+                leftImage.Visibility = Visibility.Hidden;
+                leftImage.Source = new BitmapImage(new Uri("Resources/non_smoker_brain.png", UriKind.Relative));
+                leftImage.Visibility = Visibility.Visible;
+
+                rightImage.Visibility = Visibility.Hidden;
+                rightImage.Source = new BitmapImage(new Uri("Resources/smoker_brain.png", UriKind.Relative));
+                rightImage.Visibility = Visibility.Visible;
+            } else if (drugListValue == "Cocaine")
+            {
+                leftImage.Visibility = Visibility.Hidden;
+                leftImage.Source = new BitmapImage(new Uri("Resources/non_cocaine_brain.png", UriKind.Relative));
+                leftImage.Visibility = Visibility.Visible;
+
+                rightImage.Visibility = Visibility.Hidden;
+                rightImage.Source = new BitmapImage(new Uri("Resources/cocaine_brain.png", UriKind.Relative));
+                rightImage.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Rehab_Button_Click(object sender, RoutedEventArgs e)
+        {
+            leftImage.Visibility = Visibility.Hidden;
+            rightImage.Visibility = Visibility.Hidden;
+            drugLabel.Visibility = Visibility.Hidden;
+            rehabButton.Visibility = Visibility.Hidden;
+            drugList.Visibility = Visibility.Hidden;
+
+            rehabSlider.Visibility = Visibility.Visible;
+            rehabImageRight.Visibility = Visibility.Visible;
+            rehabImageLeft.Visibility = Visibility.Visible;
+            rehabImageLeft.Source = new BitmapImage(new Uri("Resources/normal_brain.png", UriKind.Relative));
+            drugButton.Visibility = Visibility.Visible;
+            rehabLabel.Visibility = Visibility.Visible;
+
+        }
+        
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            /*
+            if (rehabSlider.Value == 1)
+            {
+                rehabImageRight.Visibility = Visibility.Hidden;
+                rehabImageRight.Source = new BitmapImage(new Uri("Resources/brain_on_meth_1month.PNG", UriKind.Relative));
+                rehabImageRight.Visibility = Visibility.Visible;
+            } else if (rehabSlider.Value == 2)
+            {
+                rehabImageRight.Visibility = Visibility.Hidden;
+                rehabImageRight.Source = new BitmapImage(new Uri("Resources/brain_on_meth_14month.PNG", UriKind.Relative));
+                rehabImageRight.Visibility = Visibility.Visible;
+            }
+            */
+        }
+        
+        private void drugButton_Click(object sender, RoutedEventArgs e)
+        {
+            leftImage.Visibility = Visibility.Visible;
+            rightImage.Visibility = Visibility.Visible;
+            drugLabel.Visibility = Visibility.Visible;
+            rehabButton.Visibility = Visibility.Visible;
+            drugList.Visibility = Visibility.Visible;
+
+            rehabSlider.Visibility = Visibility.Hidden;
+            rehabImageRight.Visibility = Visibility.Hidden;
+            rehabImageLeft.Visibility = Visibility.Hidden;
+            drugButton.Visibility = Visibility.Hidden;
+            rehabLabel.Visibility = Visibility.Hidden;
         }
     }
 }
